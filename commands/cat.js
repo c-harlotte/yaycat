323c35660aba7c8280b4a444d1e7351774d3df47
loud <glenbedwell@gmail.com>
2021-06-18 15:14:24 -0400

    feat: use let instead of var

--- commands/cat.js
+++ commands/cat.js
@@ -1,11 +1,11 @@
 const { default: axios } = require("axios");
 const Discord = require("discord.js");
-const { get } = require("snekfetch");
+const { get } = require("snekfetch")
 
 module.exports.run = async (bot, message, args) => {
   try {
     let resp = axios.get("https://catfact.ninja/fact").then((resp) => {
-      var fact = resp.data.fact;
+      let fact = resp.data.fact;
       get("https://aws.random.cat/meow").then((res) => {
         const embed = new Discord.MessageEmbed()
           .setImage(res.body.file)
