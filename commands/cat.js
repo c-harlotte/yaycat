32a4e8f18e215657b41deeadc318f5e5fc5b1e2e
loud <glenbedwell@gmail.com>
2021-06-18 15:02:25 -0400

    feat: merge cat & catfact command & make ping work

--- commands/cat.js
+++ commands/cat.js
@@ -1,11 +1,15 @@
+const { default: axios } = require("axios");
 const Discord = require("discord.js");
 const { get } = require("snekfetch"); 
 
 module.exports.run = async (bot, message, args) => {
     try {
+      let resp = axios.get("https://catfact.ninja/fact")
+      let fact = resp.data.fact
       get('https://aws.random.cat/meow').then(res => {
         const embed = new Discord.MessageEmbed()
         .setImage(res.body.file)
+        .setDescription()
         return message.channel.send({embed});
       });
     } catch(err) {
--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,16 @@
-const Discord = require("discord.js");
+const Discord, { MessageEmbed } = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-               message.channel.send('pong');
+  let messag = message.channel.send('Getting ping...').then(message => {
+    let pingEmbed = new MessageEmbed()
+    .setTitle('Pong!')
+    .addField({
+      name: "WS ping",
+      value: `${bot.ws.ping}ms`
+    });
+    message.edit("");
+    message.edit({ embed: pingEmbed });
+  })
 }
 
 module.exports.help = {
