589a84296139346289412f4336022b701d5aaccb
loud <glenbedwell@gmail.com>
2021-06-18 16:05:06 -0400

    feat: use axios instead of r2

--- commands/cat.js
+++ commands/cat.js
@@ -1,18 +1,19 @@
 const { default: axios } = require("axios");
 const Discord = require("discord.js");
-const r2 = require("r2");
 
 module.exports.run = async (bot, message, args) => {
-  let response = await r2.get("https://api.thecatapi.com/v1/images/search")
-    .json;
   try {
     let resp = axios.get("https://catfact.ninja/fact").then((resp) => {
-      let fact = resp.data.fact;
-      const embed = new Discord.MessageEmbed()
-        .setImage(response[0].url)
-        .setDescription(fact)
-        .setColor("RANDOM");
-      return message.channel.send({ embed });
+      let response = axios
+        .get("https://api.thecatapi.com/v1/images/search")
+        .then((response) => {
+          let fact = resp.data.fact;
+          const embed = new Discord.MessageEmbed()
+            .setImage(response.data[0].url)
+            .setDescription(fact)
+            .setColor("RANDOM");
+          return message.channel.send({ embed });
+        });
     });
   } catch (err) {
     return console.log(err.stack);
