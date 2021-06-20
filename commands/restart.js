c759aaef310dd31c4b3756111cb68ae9ff470769
charlotte <charlotte@ahegao.agency>
2021-06-20 16:32:01 +0300

    feat: added restart

---
+++ commands/restart.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+
+module.exports.run = async (bot, message, args) => {
+  if(!config.maintainers.includes(msg.author.id)) return;
+    const embed = new Discord.MessageEmbed()
+        .setAuthor('Restarting...', 'https://cdn.discordapp.com/emojis/585682049172635649.gif?v=1')
+        .setDescription(`I'll be back in a sec!`)
+        .setColor(0xff0000)
+
+    msg.channel.send(embed).then(function(m) {
+        require('child_process').exec("pm2 restart app", function() {
+            bot.destroy();
+            process.exit();
+        });
+        const embed2 = new Discord.MessageEmbed()
+            .setTitle('Done!')
+            .setDescription(`I'm back!`)
+            .setColor(0x00ff00)
+            
+        m.edit(embed2)
+    })
+}
+
+module.exports.help = {
+  name: "restart",
+
+  description: "restart the bot (FOR CHARLOTTE ONLY)",
+}
