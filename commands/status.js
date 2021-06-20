c51000c2568907e920804b750d12165a3d517fdc
charlotte <charlotte@ahegao.agency>
2021-06-20 16:46:44 +0300

    tseting UPDATe

--- commands/status.js
+++ commands/status.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  message.channel.send("if you can read this im online TEST COMAMND");
+  message.channel.send("if you can read this im online");
 };
 
 module.exports.help = {
